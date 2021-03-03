5aec73b2d001da1e59b4c9f358b496fbaeea77d5
Frank Fella <frank@techelevator.com>
2021-03-03 05:43:14 -0500

    Instructor Push 03/03/2021 05:43:14

---
+++ Module-2-Capstone/java/tenmo-client/src/main/java/com/techelevator/tenmo/services/TenmoApplicationServices.java
@@ -0,0 +1,11 @@
+package com.techelevator.tenmo.services;
+
+/*******************************************************************************************************
+ * This is where you code Application Services required by your solution
+ * 
+ * Remember:  theApp ==> ApplicationServices  ==>  Controller  ==>  DAO
+********************************************************************************************************/
+
+public class TenmoApplicationServices {
+
+}
